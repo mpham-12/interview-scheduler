9c1c3ed5b98107fbdf48e40eadb3f988b82d449c
mpham-12 <martinp0512@gmail.com>
2022-01-25 13:50:10 -0500

    refractored

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,104 +1,49 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import Button from "./Button";
 
 import "components/Application.scss";
-import DayList from "./DayList";
 import "components/Appointment";
+import DayList from "./DayList";
 import Appointment from "components/Appointment";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
-
+import useApplicationData from "hooks/useApplicationData";
 
 
 export default function Application(props) {
 
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
 
-  function bookInterview(id, interview) {
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
 
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-
-    return axios.put(`/api/appointments/${id}`, appointment)
-      .then((res) => {
-        console.log(res);
-        setState((prev) => ({ ...prev, appointments }));
-      })
-      .catch((err) => {
-        console.log(err);
-        throw err;
-      })
-  }
-
-  function cancelInterview(id) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    }
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    }
-
-    return axios.delete(`/api/appointments/${id}`, appointment)
-      .then((res) => setState((prev) => ({ ...prev, appointments })))
-  }
+  const interviewers = getInterviewersForDay(state, state.day);
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
+
   const schedule = dailyAppointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
 
     return (
       <Appointment
+        // key={appointment.id}
+        // id={appointment.id}
+        // time={appointment.time}
+        // interview={interview}
+        // bookInterview={bookInterview}
+        // interviewers={interviewers}
+        // cancelInterview={cancelInterview}
         key={appointment.id}
-        id={appointment.id}
-        time={appointment.time}
-        interview={interview}
+        {...appointment}
+        interview={getInterview(state, appointment.interview)}
+        interviewers={interviewers}
         bookInterview={bookInterview}
-        interviewers={getInterviewersForDay(state, state.day)}
         cancelInterview={cancelInterview}
       />
     );
   });
-
-
-
-
-  function setDay(dayName) {
-    setState({ ...state, day: dayName })
-  }
-
-
-
-
-  useEffect(() => {
-    Promise.all([
-      axios.get('/api/days'),
-      axios.get('/api/appointments'),
-      axios.get('/api/interviewers'),
-    ]).then((all) => {
-      setState(prev => ({
-        ...prev,
-        days: all[0].data,
-        appointments: all[1].data,
-        interviewers: all[2].data
-      })
-      )
-    }
-    )
-  }, [])
 
   return (
     <main className="layout">
@@ -112,8 +57,11 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList
             days={state.days}
+            day={state.day}
             value={state.day}
             onChange={setDay}
+            bookInterview={bookInterview}
+          cancelInterview={cancelInterview}
           />
         </nav>
         <img
@@ -123,12 +71,10 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* {dailyAppointments.map(app => {
-          return <Appointment key={app.id} {...app} />
-        })} */}
+
         {schedule}
 
-        <Appointment key="last" time="5pm" />
+        <Appointment key="last" time="5pm" bookInterview={bookInterview} cancelInterview={cancelInterview} />
       </section>
       <Button></Button>
     </main>
